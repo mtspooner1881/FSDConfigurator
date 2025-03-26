84165d05cb6fe74fe77242b515f7c4dce428b1c7
Mark Spooner <mkspspooner@gmail.com>
2025-03-26 13:01:57 -0500

    Updated file structure

--- src/pages/api/unitAssignment.ts
+++ src/pages/api/unitAssignment.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { UnitCardType } from '../types/unitCardTypes'
+import type { UnitCardType } from '../types/cardTypes'
 import * as data from '../../stubData/socratesStubData.json';
 
 export default function handler(
--- src/pages/unitAssignment/index.tsx
+++ src/pages/unitAssignment/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link'
-import UnitCard from '../components/unitCard/UnitCard';
+import UnitCard from '../components/hoc/UnitCard';
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -15,14 +15,12 @@ const geistMono = Geist_Mono({
 
 export default function unitAssignment(): React.JSX.Element {
   const [unitCards, setUnitCards] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetch('/api/unitAssignment')
       .then((res) => res.json())
       .then((data) => {
-        setUnitCards(data)
-        setIsLoading(false)
-      })
+        setUnitCards(data);
+      });
   }, [])
 
   return (
